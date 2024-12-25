c97c01f76c9d2c6cabafc1dbab85e94da9409677
imanhaikal <imanhaikal04@gmail.com>
2024-12-25 20:17:38 +0800

    gradle sync

--- SharePlateApp/build.gradle.kts
+++ SharePlateApp/build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.2.2")
+        classpath("com.android.tools.build:gradle:8.7.3")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
         classpath("com.google.gms:google-services:4.4.2")
     }
@@ -15,11 +15,4 @@ plugins {
     alias(libs.plugins.android.application) apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
 }
