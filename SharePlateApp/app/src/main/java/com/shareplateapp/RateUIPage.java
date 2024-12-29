667a7f3dbd3fd391853da5a1b61d15d05c270da1
reencodingg <nutzreen@gmail.com>
2024-12-29 21:19:49 +0800

    rating dialog

--- SharePlateApp/app/src/main/java/com/shareplateapp/RateUIPage.java
+++ SharePlateApp/app/src/main/java/com/shareplateapp/RateUIPage.java
@@ -1,63 +1,86 @@
 package com.shareplateapp;
 
+import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
+import android.widget.ImageView;
+import android.widget.RatingBar;
 
-import androidx.fragment.app.Fragment;
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.AppCompatButton;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link RateUIPage#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class RateUIPage extends Fragment {
+public class RateUIPage extends Dialog {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public RateUIPage() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RateUIPage.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static RateUIPage newInstance(String param1, String param2) {
-        RateUIPage fragment = new RateUIPage();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    private float userRate = 0;
+    public RateUIPage(@NonNull Context context) {
+        super(context);
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        setContentView(R.layout.fragment_rate_u_i_page);
+
+        final AppCompatButton rateNowBtn = findViewById(R.id.rateNowBtn);
+        final AppCompatButton laterBtn = findViewById(R.id.laterBtn);
+        final RatingBar ratingBar = findViewById(R.id.ratingBar);
+        final ImageView ratingImage = findViewById(R.id.ratingImage);
+
+        rateNowBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //
+            }
+        });
+
+        laterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // hide rating dialog
+                dismiss();
+            }
+        });
+
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+
+                if(rating <= 1) {
+                    ratingImage.setImageResource(R.drawable.one_star);
+                }
+                else if(rating <= 2) {
+                    ratingImage.setImageResource(R.drawable.two_star);
+                }
+                else if(rating <= 3) {
+                    ratingImage.setImageResource(R.drawable.three_star);
+                }
+                else if(rating <= 4) {
+                    ratingImage.setImageResource(R.drawable.four_star);
+                }
+                else {
+                    ratingImage.setImageResource(R.drawable.five_star);
+                }
+
+                // animate emoji image
+                animateImage(ratingImage);
+
+                // selected rating by user
+                userRate = rating;
+            }
+        });
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_rate_u_i_page, container, false);
+    private void animateImage(ImageView ratingImage) {
+
+        ScaleAnimation scaleAnimation = new ScaleAnimation(0, 1f, 0, 1f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+
+        scaleAnimation.setFillAfter(true);
+        scaleAnimation.setDuration(200);
+        ratingImage.startAnimation(scaleAnimation);
     }
 }
